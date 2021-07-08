72ae6d40d7ba621a28580f8b557fe466a3634d8b
Mohamed Ahamed <mohamedx.28@gmail.com>
2021-07-08 06:59:16 +0100

    #Change Store API from products to updateProduct

--- app/Http/Controllers/StoreProductsController.php
+++ app/Http/Controllers/StoreProductsController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Helper\ResponseMessage as Resp;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\UpdateProductRequest;
+use App\Models\Product;
+use Illuminate\Support\Str;
 
 class StoreProductsController extends Controller
 {
@@ -23,6 +26,33 @@ class StoreProductsController extends Controller
             return Resp::Success('تم', $data);
         } catch (\Throwable $th) {
             //throw $th;
+            return Resp::Error('حدث خطأ ما', $th->getMessage());
+        }
+    }
+
+    public function update(UpdateProductRequest $request, Product $product)
+    {
+        if (!$product->store->where('store_products.user_id', auth()->user()->id)->exists()) {
+            return Resp::Error('لا تملك هذا المنتج', null);
+        }
+
+
+        $validate = $request->validated();
+
+        foreach ($validate as $key => $value) {
+            $product->$key = $value;
+        }
+
+        if (isset($request['photo'])) {
+            $product->photo = Str::of($request->file('photo')->storePublicly('Product'));
+        }
+
+        try {
+
+            $product->save();
+
+            return Resp::Success('تم التحديث', $product);
+        } catch (\Throwable $th) {
             return Resp::Error('حدث خطأ ما', $th->getMessage());
         }
     }
--- routes/api.php
+++ routes/api.php
@@ -174,7 +174,7 @@ Route::group(['middleware' => 'auth.jwt'], function () {
     Route::group(['prefix' => BASE, 'middleware' => 'storeWare'], function () {
 
         // store product
-        Route::post('products/{product}', [StoreProductsControllerResource::class, 'update']);
+        Route::post('updateProduct/{product}', [StoreProductsController::class, 'update']);
         Route::ApiResource('product', StoreProductsControllerResource::class)->except('update');
 
         // store additional description
